4bdc5939c9a35eab5abc0851c0d8565241347a12
kuuy <master@kuuy.com>
2022-10-17 16:46:54 +0800

    pytorch tts next googlenet model

--- cryptos/commands/ai/tts/__init__.py
+++ cryptos/commands/ai/tts/__init__.py
@@ -3,5 +3,7 @@ from flask import Blueprint
 bp = Blueprint('tts', __name__)
 
 def register_blueprint():
-    from . import tacotron2
-    bp.register_blueprint(tacotron2.bp)
+  from . import (
+    tacotron2,
+  )
+  bp.register_blueprint(tacotron2.bp)
--- cryptos/commands/ai/tts/tacotron2.py
+++ cryptos/commands/ai/tts/tacotron2.py
@@ -9,6 +9,18 @@ def mix():
   torch.random.manual_seed(0)
   device = "cuda" if torch.cuda.is_available() else "cpu"
 
-  print(torch.__version__)
-  print(torchaudio.__version__)
-  print(device)
+  bundle = torchaudio.pipelines.TACOTRON2_WAVERNN_PHONE_LJSPEECH
+  processor = bundle.get_text_processor()
+  tacotron2 = bundle.get_tacotron2().to(device)
+  vocoder = bundle.get_vocoder().to(device)
+
+  text = "who register for a Binance account"
+
+  with torch.inference_mode():
+    processed, lengths = processor(text)
+    processed = processed.to(device)
+    lengths = lengths.to(device)
+    spec, spec_lengths, _ = tacotron2.infer(processed, lengths)
+    waveforms, lengths = vocoder(spec, spec_lengths)
+
+  torchaudio.save("output_wavernn.wav", waveforms[0:1].cpu(), sample_rate=vocoder.sample_rate)
